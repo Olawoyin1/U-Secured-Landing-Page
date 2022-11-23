41410453828fcee70fa8052fca3dbcc80f9a8ea8
Gbolahan <yustee2017@gmai.com>
2022-11-24 00:34:09 +0100

    init

--- src/pages/HeroComponents/Showcase.jsx
+++ src/pages/HeroComponents/Showcase.jsx
@@ -13,13 +13,20 @@ const Showcase = () => {
         <div className="hero-content">
             <h1 className='bold'>Safety made just for <span className="color">you</span></h1>
             <p>The Safety tech built to keep you safe and connected with other users around the globe</p>
-            <button className="btn">Join The Waitlist</button>
+            <div className='waitlist-arrow'>
+              <button className="btn">Join The Waitlist</button>
+              
+              <a href="" className=' arrow-mobile'>
+                <i className="uil uil-arrow-down"></i>
+              </a>
+
+            </div>
         </div>
 
           <img src="images/hero-image.png" alt="" />
          
 
-          <a href="" className='arrow'>
+          <a href="" className='arrow arrow-pc'>
             <i className="uil uil-arrow-down"></i>
           </a>
       </div>
