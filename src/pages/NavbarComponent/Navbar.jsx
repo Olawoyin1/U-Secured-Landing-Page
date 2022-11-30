7da4093fbbf00e8ecca283c05fa7c672e34460cc
Gbolahan <yustee2017@gmai.com>
2022-11-30 17:17:08 +0100

    init

--- src/pages/NavbarComponent/Navbar.jsx
+++ src/pages/NavbarComponent/Navbar.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 import { NavLink , Link} from 'react-router-dom'
 import "./navbar.css"
 import { HiBars3BottomRight } from "react-icons/hi2";
+import { HiOutlineX } from "react-icons/hi";
 
 const Navbar = () => {
+
+  const [openNav, setOpenNav] = React.useState(true)
+
   return (
     <div className="navbar">
         <div className="container navbar-container">
@@ -16,15 +20,22 @@ const Navbar = () => {
           </Link>
             
            
-          <ul className="links">
-              <li className="first"><a href="#about">About Us</a></li>
-              <li><a href="">Features</a></li>
-              <li><a href="">News</a></li>
-              <li><a href="">Contact</a></li>
-              <Link to={"join-waitlist"} className="btn last">Join the waitlist</Link>
+          <ul className={openNav ? "links" : "links show"}>
+              <li className="first"><a onClick={()=> setOpenNav(true)} href="#about">About Us</a></li>
+              <li><a onClick={()=> setOpenNav(true)} href="">Features</a></li>
+              <li><a onClick={()=> setOpenNav(true)} href="">News</a></li>
+              <li><a onClick={()=> setOpenNav(true)} href="">Contact</a></li>
+              <li><Link to={"join-waitlist"} onClick={()=> setOpenNav(true)} className="btn last">Join the waitlist</Link></li>
+              <HiOutlineX 
+                className="close-btn" 
+                onClick={()=> setOpenNav(true)}
+              />
           </ul>
 
-          <HiBars3BottomRight className='menu' />
+          <HiBars3BottomRight 
+            className='menu' 
+            onClick={()=> setOpenNav(false)}
+          />
 
       
         </div>
